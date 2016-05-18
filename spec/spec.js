caf59e6e7107fd8c9f6acb35166b37df7e73938e
Tee Blagdon <tjlay23@gmail.com>
2016-05-18 13:45:36 -0700

    Add test for .append()

--- spec/spec.js
+++ spec/spec.js
@@ -2,11 +2,60 @@
   'use strict';
 
   describe('singly linked lists', function() {
-    describe('fn1', function() {
-      it('req 1', function() {
-        expect(1).to.equal(2)
+     
+    var nums = new LinkedList();
+
+    describe('LinkedList Class', function() {
+
+      //test groupings:
+      //linked list constructor
+      it('should initiate a Linked List Class with length 0', function() {
+        expect(nums.length).to.equal(0);
       });
+
     });
+
+    describe('append', function() {
+      it('should append new keys to linked list', function() {
+        nums.append('zero');
+
+        expect(nums.head.key).to.equal('zero');
+        expect(nums.length).to.equal(1);
+        expect(nums.append('one')).to.equal(true);
+        expect(nums.length).to.equal(2);
+      });
+
+    });
+
+    describe('remove', function() {
+
+
+    });
+
+    describe('insert', function() {
+
+    });
+    
+
+    describe('removeAt', function() {
+      
+    });
+
+
+    describe('insertAt', function() {
+      
+    });
+
+
+    describe('indexOf', function() {
+      
+    });
+
+
+    describe('toArray', function() {
+      
+    });
+
   });
 
   describe('doubly linked lists', function() {
