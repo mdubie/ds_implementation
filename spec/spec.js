4777b0bd0feeebc405ed1f2d4869c58384c13436
Matt Dubie <mbdubie@gmail.com>
2016-05-19 11:46:40 -0400

    added additional function (removeAt and toArray) to list Method completed testing for add and value at

--- spec/spec.js
+++ spec/spec.js
@@ -1,13 +1,134 @@
+// add
+// valueAt
+// insertAt
+// toArray
+// removeAt
+
 (function() {
   'use strict';
 
   describe('singly linked lists', function() {
-    describe('fn1', function() {
+    describe('add', function() {
+      it('if list is empty, it should add first node', function() {
+        var listAdd1 = new List();
+        listAdd1.add('pizza');
+
+        var refAdd1 = {length:1, head:{ value:'pizza', next: null } };
+
+        expect(listAdd1.head).to.deep.eql(refAdd1.head)
+      });
+
+      it('if list is not empty, it should add at end of list', function() {
+        var listAdd2 = new List();
+        listAdd2.add('pizza');
+        listAdd2.add('veggies');
+
+        var refAdd2 = { length: 2, head: { value: 'pizza', next: { value: 'veggies', next: null } } }
+
+        expect(listAdd2.head).to.deep.eql(refAdd2.head)
+      });
+
+      it('it should properly increment length counter', function() {
+        var listAdd3 = new List();
+        expect(listAdd3.length).to.equal(0)
+
+        listAdd3.add('chocolate');
+        expect(listAdd3.length).to.equal(1)
+      });
+    });
+
+    describe('valueAt', function() {
+      it('if list length is 1, should return first item', function() {
+        var listValueAt1 = new List();
+        listValueAt1.add('pizza');
+
+        expect(listValueAt1.valueAt(0)).to.equal('pizza')
+      });
+
+      it('if list length is greater than 1, should return that item', function() {
+        var listValueAt2 = new List();
+        listValueAt2.add('pizza');
+        listValueAt2.add('rain');
+        listValueAt2.add('cheese');
+
+        expect(listValueAt2.valueAt(2)).to.equal('cheese')
+      });
+
+      it('if invalid index is given, should let user know there is an error', function() {
+        var listValueAt3 = new List();
+        listValueAt3.add('pizza');
+        listValueAt3.add('rain');
+        listValueAt3.add('cheese');
+
+        expect(listValueAt3.valueAt(5)).to.equal('Invalid index')
+      });
+    });
+    
+
+    describe('insertAt', function() {
+      it('when called at index 0, should insert node at beginning of list', function() {
+        var listInsertAt1 = new List();
+        listInsertAt1.add('pizza');
+        listInsertAt1.add('rain');
+        listInsertAt1.insertAt(0, 'potatoe')
+
+        var refHead = { value: 'pizza', next: { value: 'potatoe', next: { value: 'rain', next: null } } }
+
+        expect(listInsertAt1.head).to.deep.eql(refHead)
+      });
+
+      it('when called with a middle index, should insert node into list at given index', function() {
+        var listInsertAt1 = new List();
+        listInsertAt1.add('pizza');
+        listInsertAt1.add('rain');
+        listInsertAt1.insertAt(1, 'potatoe')
+
+        var refHead = { value: 'pizza', next: { value: 'potatoe', next: { value: 'rain', next: null } } }
+
+        expect(listInsertAt1.head).to.deep.eql(refHead)
+      });
+
+      it('when called with the last index, should insert node onto the end of the list', function() {
+        var listInsertAt1 = new List();
+        listInsertAt1.add('pizza');
+        listInsertAt1.add('rain');
+        listInsertAt1.insertAt(1, 'potatoe')
+
+        var refHead = { value: 'pizza', next: { value: 'potatoe', next: { value: 'rain', next: null } } }
+
+        expect(listInsertAt1.head).to.deep.eql(refHead)
+      });
+
+      it('should increment the length of the list accordingly', function() {
+        var listInsertAt1 = new List();
+        listInsertAt1.add('pizza');
+        listInsertAt1.add('rain');
+        listInsertAt1.insertAt(1, 'potatoe')
+
+        var refHead = { value: 'pizza', next: { value: 'potatoe', next: { value: 'rain', next: null } } }
+
+        expect(listInsertAt1.head).to.deep.eql(refHead)
+      });
+
+    });
+    
+
+    describe('toArray', function() {
       it('req 1', function() {
         expect(1).to.equal(2)
       });
     });
+    
+
+    describe('removeAt', function() {
+      it('req 1', function() {
+        expect(1).to.equal(2)
+      });
+    });
+    
   });
+
+
 
   describe('doubly linked lists', function() {
     describe('fn1', function() {
--- src/singlylinked.js
+++ src/singlylinked.js
@@ -1,38 +1,117 @@
-/**
- * Brain storm of methods and standard array methods
- * add
- * addN
- * index (access n value)
- * remove
- * replace (change value)
- * toArray
- * fromArray
- * length
- * print (in user friendly manner)
- * concat()
- * every()
- * fill()
- * filter()
- * find()
- * findIndex()
- * forEach()
- * indexOf()
- * isArray()
- * join()
- * lastIndexOf()
- * map()
- * pop()
- * push()
- * reduce()
- * reduceRight()
- * reverse()
- * shift()
- * slice()
- * some()
- * sort()
- * splice()
- * unshift()
- * removeDuplicates
- *
- * 
- */
+
+ var List = function() {
+   this.length = 0;
+   this.head = null;
+ }
+
+ var Node = function(value) {
+   this.value = value;
+   this.next = null;
+ }
+
+ List.prototype.add = function(value) {
+   var curNode = new Node(value);
+   var pointer = this.head;
+   
+   this.length += 1;
+   
+   if (!pointer) {
+     this.head = curNode;
+   } else {
+     while (pointer.next) {
+       pointer = pointer.next;
+     }
+     pointer.next = curNode;
+   }
+ }
+
+ List.prototype.valueAt = function(index) {
+   var pointer = this.head;
+   
+   if (index < 0 || index >= this.length) { //edge con
+     return 'Invalid index'
+   }
+
+   for (var i = 0; i < index; i++) { //edge here
+     pointer = pointer.next;
+   }
+   return pointer.value;
+ }
+
+ List.prototype.insertAt = function(index, value) {  //index of 0 === this.head
+   var pointer = this.head;
+   
+   if (index < 0 || index >= this.length) { 
+     return 'Invalid index'
+   }
+
+   for (var i = 0; i < index-1; i++) { 
+     pointer = pointer.next;
+   }
+
+   this.length += 1;
+   var ref = pointer.next;
+   var insertNode = new Node(value);
+   pointer.next = insertNode;
+   pointer.next.next = ref;   //<==removed a line of code here
+ }
+
+ var listInsertAt1 = new List();
+ listInsertAt1.add('pizza');
+ listInsertAt1.add('rain');
+ listInsertAt1.insertAt(0, 'potatoe')
+ console.log('1',listInsertAt1)
+ var listInsertAt2 = new List();
+ listInsertAt2.add('pizza');
+ listInsertAt2.add('rain');
+ listInsertAt2.insertAt(2, 'potatoe')
+ console.log('2', listInsertAt2)
+
+ List.prototype.toArray = function() {
+ 	var result = [];
+ 	var pointer = this.head;
+ 	while(pointer) {
+ 		result.push(pointer.value);
+ 		pointer = pointer.next;
+ 	}
+ 	return result;
+ }
+
+ // var list = new List();
+ // list.add('pizza');
+ // list.add('cheese');
+ // list.add('veggies');
+ // list.add('pasta');
+ // console.log(list);
+ // console.log(list.toArray())
+ 
+
+List.prototype.removeAt = function(index) {
+	this.length -= 1;
+	var pointer = this.head;
+
+	if (index < 0 || index >= this.length) { 
+	  return 'Invalid index'
+	}
+
+	for (var i = 0; i < index-1; i++) { 
+	  pointer = pointer.next;
+	}
+
+	var ref = pointer.next.next;
+	pointer.next = ref;
+};
+
+// var list = new List();
+// list.add('pizza');
+// list.add('cheese');
+// list.add('veggies');
+// list.add('pasta');
+// console.log(list.head);
+// list.removeAt(1)
+// console.log(list.head);
+
+
+
+
+
